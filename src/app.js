2cea44f8baf90232b8279e2c7e0823695a2ab2c2
Divya Palanivel <divyadreams2002@gmail.com>
2026-05-16 14:27:10 +0530

    explored http methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send('Hello, World from main page!');
-})
+// app.use("/test/1",(req, res) => {
+//     res.send('Hello, World from test-1!');
+// })
+
+// app.use("/", (req, res) => {
+//     res.send('Hello, World from main!');
+// })
 
 app.use("/test",(req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World from test!');
 })
 
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+app.get("/test", (req, res) => {
+  res.send("Response sent");
 });
 
+app.post("/test", (req, res) => {
+  res.send("Data saveed to DB");
+});
+
+app.delete("/test", (req, res) => {
+  res.send("Deleted ");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
