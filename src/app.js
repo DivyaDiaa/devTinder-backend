b77003bc36c1027a7c8b88a3fa641f18ca97f1fc
Divya Palanivel <divyadreams2002@gmail.com>
2026-05-18 16:32:52 +0530

    added validations

--- src/app.js
+++ src/app.js
@@ -6,25 +6,25 @@ const app = express();
 
 app.use(express.json()); //will change json to js object
 
-// app.post("/signup", async (req, res) => {
-//   console.log(req.body);
-//   //here it will come undefined as you need middleware becuase data will be coming in Json format
-//   // const userObj = {
-//   //   firstName: "Siva",
-//   //   lastName: "Chandran",
-//   //   emailId: "siva2000@gmail.com",
-//   //   password: "Siva@1904",
-//   // };
-//   const user = new userModel(req.body);
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  //here it will come undefined as you need middleware becuase data will be coming in Json format
+  // const userObj = {
+  //   firstName: "Siva",
+  //   lastName: "Chandran",
+  //   emailId: "siva2000@gmail.com",
+  //   password: "Siva@1904",
+  // };
+  const user = new userModel(req.body);
 
-//   try {
-//     // const user = new userModel(userObj);
-//     await user.save();
-//     res.send("Database connected ");
-//   } catch (err) {
-//     res.status(400).send("Error saving the user: " + err.message);
-//   }
-// });
+  try {
+    // const user = new userModel(userObj);
+    await user.save();
+    res.send("Database connected ");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
 
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
@@ -64,13 +64,13 @@ app.patch("/user", async (req, res) => {
     const updateUser = await userModel.findByIdAndUpdate(
       req.body._id,
       req.body,
-      { returnDocument: "after" }, //options argumnet
+      { returnDocument: "after", runValidators: true }, //options argumnet
     );
     console.log(updateUser); //will give before update data if u need current updateed data
     //please add 3rd argument
     res.send("User updated");
   } catch (err) {
-    res.status(400).send("Soemthing went wrong");
+    res.status(400).send("Something went wrong "+ err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,47 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("User is not valid human");
+        }
+      }, //validate fucntion will onky work for new documents adding
+      //for making it to wrk in patch update u need to call runValidators in patch as options argumnet
+    },
+    location: {
+      type: String,
+      default: "India",
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true, //updatedAt
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-});
+);
 
 const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
