9727c759b20caf8e2ec340fda036e658fdac8718
Divya Palanivel <divyadreams2002@gmail.com>
2026-05-18 14:50:54 +0530

    http methods

--- src/app.js
+++ src/app.js
@@ -6,25 +6,25 @@ const app = express();
 
 app.use(express.json()); //will change json to js object
 
-app.post("/signup", async (req, res) => {
-  console.log(req.body);
-  //here it will come undefined as you need middleware becuase data will be coming in Json format
-  // const userObj = {
-  //   firstName: "Siva",
-  //   lastName: "Chandran",
-  //   emailId: "siva2000@gmail.com",
-  //   password: "Siva@1904",
-  // };
-  const user = new userModel(req.body);
+// app.post("/signup", async (req, res) => {
+//   console.log(req.body);
+//   //here it will come undefined as you need middleware becuase data will be coming in Json format
+//   // const userObj = {
+//   //   firstName: "Siva",
+//   //   lastName: "Chandran",
+//   //   emailId: "siva2000@gmail.com",
+//   //   password: "Siva@1904",
+//   // };
+//   const user = new userModel(req.body);
 
-  try {
-    // const user = new userModel(userObj);
-    await user.save();
-    res.send("Database connected ");
-  } catch (err) {
-    res.status(400).send("Error saving the user: " + err.message);
-  }
-});
+//   try {
+//     // const user = new userModel(userObj);
+//     await user.save();
+//     res.send("Database connected ");
+//   } catch (err) {
+//     res.status(400).send("Error saving the user: " + err.message);
+//   }
+// });
 
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
@@ -42,7 +42,33 @@ app.get("/user", async (req, res) => {
 app.get("/feed", async (req, res) => {
   try {
     const allData = await userModel.find({});
+    console.log(allData);
     res.send(allData);
+  } catch (err) {
+    res.status(400).send("Soemthing went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const deleteUser = await userModel.findByIdAndDelete({ _id: req.body._id });
+    res.send("User deleted");
+  } catch (err) {
+    res.status(400).send("Soemthing went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  console.log(req.body);
+  try {
+    const updateUser = await userModel.findByIdAndUpdate(
+      req.body._id,
+      req.body,
+      { returnDocument: "after" }, //options argumnet
+    );
+    console.log(updateUser); //will give before update data if u need current updateed data
+    //please add 3rd argument
+    res.send("User updated");
   } catch (err) {
     res.status(400).send("Soemthing went wrong");
   }
